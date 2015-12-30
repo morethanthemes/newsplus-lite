e177c2e95871ecf0ab14ae57f643de332322128c
Bill Kechagias <vkechagias@gmail.com>
2015-12-30 17:27:37 +0200

    feat(back-to-top)Back to top feature initial implementation

---
+++ site/themes/contrib/newsplus_lite/js/custom/back-to-top.js
@@ -0,0 +1,17 @@
+  /**
+  * Add Javascript - Back To Top
+  */
+jQuery(document).ready(function($) { 
+  $(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+      $("#toTop").fadeIn(); 
+    } else {
+      $("#toTop").fadeOut();
+    }
+  });
+  
+  $("#toTop").click(function() {
+    $("body,html").animate({scrollTop:0},800);
+  }); 
+  
+});
