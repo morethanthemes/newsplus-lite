b7ba818f5414715020eba2b0dc8c40ca4a1a43ae
Bill Kechagias <vkechagias@gmail.com>
2015-12-30 18:24:28 +0200

    feat(fixed-header)Initial implementation of fixed header

---
+++ site/themes/contrib/newsplus_lite/js/custom/fixed-header.js
@@ -0,0 +1,39 @@
+  /**
+  * Add Javascript - Fixed Header
+  */
+jQuery(document).ready(function($) { 
+  var preHeaderHeight = $("#pre-header").outerHeight(),
+  headerTopHeight = $("#header-top").outerHeight(),
+  headerHeight = $("#header").outerHeight();
+  $(window).load(function() {
+    if(($(window).width() > 767)) {
+      $("body").addClass("fixed-header-enabled");
+    } else {
+      $("body").removeClass("fixed-header-enabled");
+    }
+  });
+  $(window).resize(function() {
+    if(($(window).width() > 767)) {
+      $("body").addClass("fixed-header-enabled");
+    } else {
+      $("body").removeClass("fixed-header-enabled");
+    }
+  });
+  $(window).scroll(function() {
+    if(($(this).scrollTop() > preHeaderHeight+headerTopHeight+headerHeight) && ($(window).width() > 767)) {
+      $("body").addClass("onscroll");
+      if ($("#toolbar-administration").length > 0) {
+        var adminHeight = $('body').css('paddingTop');
+        $("#header").css("top", adminHeight);
+      }
+      if ($("#page-intro").length > 0) { 
+        $("#page-intro").css("paddingTop", (headerHeight)+"px");
+      } else {
+        $("#page").css("paddingTop", (headerHeight)+"px");
+      }
+    } else {
+      $("body").removeClass("onscroll");
+      $("#page,#page-intro").css("paddingTop", (0)+"px");
+    }
+  });
+});
