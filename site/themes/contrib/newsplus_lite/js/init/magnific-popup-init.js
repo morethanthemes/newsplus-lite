67aed7029224ed573826b21b27f3b4033163a51d
Bill Kechagias <vkechagias@gmail.com>
2016-06-03 17:06:07 +0300

    fix(image-popup): add title on lightbox, correctly close divs in field image twig file

--- site/themes/contrib/newsplus_lite/js/init/magnific-popup-init.js
+++ site/themes/contrib/newsplus_lite/js/init/magnific-popup-init.js
@@ -7,6 +7,11 @@ jQuery(document).ready(function($) {
       mainClass: "mfp-fade",
       gallery: {
         enabled: true, // set to true to enable gallery
+      },
+      image: {
+        titleSrc: function(item) {
+          return item.el.children()[0].title || '';
+        }
       }
     });
   });
