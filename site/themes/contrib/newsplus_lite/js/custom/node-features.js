81a58b33b39067c447d3add3132fde4e5761e0ec
Bill Kechagias <vkechagias@gmail.com>
2016-06-03 17:33:01 +0300

    feat(affix-side): improve affix bottom calculation even after resizing

--- site/themes/contrib/newsplus_lite/js/custom/node-features.js
+++ site/themes/contrib/newsplus_lite/js/custom/node-features.js
@@ -9,6 +9,7 @@ jQuery(document).ready(function($) {
       + $("#main-content").outerHeight(true)
       - $(".block-system-main-block").outerHeight(true),
       affixTop = $("#affix").offset().top;
+      var isInitialised = false;
 
       //The admin overlay menu height
       var headerHeight = $("#header").outerHeight(true);
@@ -23,11 +24,24 @@ jQuery(document).ready(function($) {
             bottom: affixBottom
           }
         });
+        isInitialised = true;
       }
 
       initializeAffix(initAffixTop);
 
+
+      function recalcAffixBottom() {
+        affixBottom = $("#footer").outerHeight(true)
+          + $("#subfooter").outerHeight(true)
+          + $("#main-content").outerHeight(true)
+          - $(".block-system-main-block").outerHeight(true);
+        $("#affix").data("bs.affix").options.offset.bottom = affixBottom;
+      }
+
       $("#affix").on("affixed.bs.affix", function () {
+        if (isInitialised) {
+          recalcAffixBottom();
+        }
         //We set through JS the inline style top position
         if ($(".user-logged-in").length>0) {
           $("#affix").css("top", (topValue)+"px");
